57a8948f9e7d2ca74dba9be3188e41cf6900a7c7
erdman3 <37053514+erdman3@users.noreply.github.com>
2018-04-07 20:58:53 -0500

    Update for Graph.java, current edition
    This is what I have come up with thus far.
    Not sure how to do the methods that return Iterable. Once I have them done, it should be apparent whether or not the code works.
    To do: Want to change the ArrayList<E> component of the HashMap to another HashSet of vertices

--- Graph.java
+++ Graph.java
@@ -1,6 +1,8 @@
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
+import java.util.ArrayList;
 
 /**
  * Undirected and unweighted graph implementation
@@ -12,37 +14,71 @@ import java.util.Set;
  */
 public class Graph<E> implements GraphADT<E> {
     
-    /**
-     * Instance variables and constructors
-     */	
-		/**
-	 * Inner class that defines graph nodes which will
-	 * be the objects associated with each vertex.
-	 * 
-	 * @author XTeam 56
-	 *
-	 * @param <E> generic data type to be used for graph
-	 */
 	class GraphNode<E> {
 		
 		E nodeData;
 		boolean visited;
-		HashMap<E,E> neighbors;
 		
 		GraphNode(E data) {
 			this.nodeData = data;
 			this.visited = false;
-			this.neighbors = new HashMap<E,E>();
+		}
+		
+		public E getData() {
+			return this.nodeData;
+		}
+		
+		public void setData(E newData) {
+			this.nodeData = newData;
+		}
+		
+		@Override
+		public boolean equals(Object graphNode) {
+	        if (((GraphNode<E>)graphNode).getData().equals(this.nodeData)){
+	        	return true;
+	        }
+	        return false;
+		}
+		
+		@Override
+		public int hashCode() {
+			return this.nodeData.hashCode();
+		}
+		
+		@Override
+		public String toString() {
+			return this.nodeData.toString();
 		}
 	}
+	
+    /**
+     * Instance variables and constructors
+     */
+	//List that holds all vertexes in the graph
+	private HashSet<GraphNode<E>> vertexList;
+	
+	//List that holds all edges in the graph
+	private HashMap<GraphNode<E>,ArrayList<GraphNode<E>>> adjacencyList;
 
+	public Graph() {
+		this.vertexList = new HashSet<GraphNode<E>>();
+		this.adjacencyList = new HashMap<GraphNode<E>,ArrayList<GraphNode<E>>>();
+		
+	}
+	
     /**
      * {@inheritDoc}
      */
     @Override
     public E addVertex(E vertex) {
-        
-    	return vertex;
+    	if(vertex != null && !this.vertexList.contains(new GraphNode<E>(vertex))) {
+    		this.vertexList.add(new GraphNode<E>(vertex));
+    		this.adjacencyList.put(new GraphNode<E>(vertex), new ArrayList<GraphNode<E>>());
+    		return vertex;
+    	}
+    	else {
+    		return null;
+    	}
     }
 
     /**
@@ -50,8 +86,15 @@ public class Graph<E> implements GraphADT<E> {
      */
     @Override
     public E removeVertex(E vertex) {
+        if(vertex == null || !this.vertexList.contains(new GraphNode<E>(vertex))) {
+        	return null;
+        }
         
-    	return vertex;
+        else {
+        	this.vertexList.remove(new GraphNode<E>(vertex));
+        	this.adjacencyList.remove(new GraphNode<E>(vertex));
+        	return vertex;
+        }
     }
 
     /**
@@ -59,7 +102,17 @@ public class Graph<E> implements GraphADT<E> {
      */
     @Override
     public boolean addEdge(E vertex1, E vertex2) {
-        return false;
+        if(!this.vertexList.contains(new GraphNode<E>(vertex1)) || 
+        		!this.vertexList.contains(new GraphNode<E>(vertex2)) || 
+        		vertex1.equals(vertex2)) {
+        	return false;
+        }
+        
+        else {
+        	this.adjacencyList.get(new GraphNode<E>(vertex1)).add(new GraphNode<E>(vertex2));
+        	this.adjacencyList.get(new GraphNode<E>(vertex2)).add(new GraphNode<E>(vertex1));
+        	return true;
+        }
     }    
 
     /**
@@ -67,7 +120,17 @@ public class Graph<E> implements GraphADT<E> {
      */
     @Override
     public boolean removeEdge(E vertex1, E vertex2) {
-        return false;
+        if(!this.vertexList.contains(new GraphNode<E>(vertex1)) || 
+        		!this.vertexList.contains(new GraphNode<E>(vertex2)) || 
+        		vertex1.equals(vertex2)) {
+        	return false;
+        }
+        
+        else {
+        	this.adjacencyList.get(new GraphNode<E>(vertex1)).remove(new GraphNode<E>(vertex2));
+        	this.adjacencyList.get(new GraphNode<E>(vertex2)).remove(new GraphNode<E>(vertex1));
+        	return true;
+        }
     }
 
     /**
@@ -75,7 +138,18 @@ public class Graph<E> implements GraphADT<E> {
      */
     @Override
     public boolean isAdjacent(E vertex1, E vertex2) {
-        return false;
+        if(!this.vertexList.contains(new GraphNode<E>(vertex1)) || 
+        		!this.vertexList.contains(new GraphNode<E>(vertex2)) || 
+        		vertex1.equals(vertex2)) {
+        	return false;
+        }
+        
+        else {
+        	if(this.adjacencyList.get(new GraphNode<E>(vertex1)).contains(new GraphNode<E>(vertex2))) {
+        		return true;
+        	}
+        	return false;
+        }
     }
 
     /**
@@ -83,7 +157,7 @@ public class Graph<E> implements GraphADT<E> {
      */
     @Override
     public Iterable<E> getNeighbors(E vertex) {
-        return (Iterable<E>) vertex;
+        return (Iterable<E>) this.adjacencyList;
     }
 
     /**
@@ -91,8 +165,11 @@ public class Graph<E> implements GraphADT<E> {
      */
     @Override
     public Iterable<E> getAllVertices() {
-        E itr = null;
-		return (Iterable<E>) itr;
+        return (Iterable<E>) this.vertexList;
     }
-
+    
+    public String hashSetToString() {
+    	System.out.println(this.adjacencyList.toString());
+		return this.vertexList.toString();
+    }
 }
