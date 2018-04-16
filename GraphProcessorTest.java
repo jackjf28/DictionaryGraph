6a1549ecf88d67b3733b0a623e39a9d2734a7f09
sfetherston <32717737+sfetherston@users.noreply.github.com>
2018-04-15 19:01:07 -0500

    Update GraphProcessorTest.java
    Start of test where shortestDistance and path are failing

--- GraphProcessorTest.java
+++ GraphProcessorTest.java
@@ -1,5 +1,6 @@
 import static org.junit.Assert.*;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,6 +14,48 @@ import org.junit.Test;
  * Junit test class to test class @see GraphProcessor
  *
  */
-public class GraphTest {
+public class GraphProcessorTest {
+    
+    private GraphProcessor<String> graph;
 
+    @Test
+    public final void createGraph() {
+        this.graph = new GraphProcessor<String>();
+    }
+    
+    @Test
+    public final void populateGraph() {
+        int numberOfWords = 4;
+        this.graph = new GraphProcessor<String>();
+        try {
+            graph.populateGraph("TestWords.txt").compareTo(numberOfWords);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+    
+    @Test
+    public final void testShortestPath() {
+        this.graph = new GraphProcessor<String>();
+        try {
+            graph.populateGraph("TestWords.txt");
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        System.out.print(graph.getShortestPath("CAT", "SAM"));
+    }
+    
+    @Test
+    public final void testShortestLength() {
+        this.graph = new GraphProcessor<String>();
+        try {
+            graph.populateGraph("TestWords.txt");
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        graph.getShortestDistance("CAT", "SAM").compareTo(3);
+    }
 }
